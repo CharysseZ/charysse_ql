0a9925d1aa7c97a9f457e4feff4feef2105e2a4b
CharysseZ <107160622+CharysseZ@users.noreply.github.com>
2024-07-29 20:40:21 +0800

    Update 通用机场签到.py

--- 通用机场签到.py
+++ 通用机场签到.py
@@ -1,36 +1,18 @@
 #只要机场网站''' Powered by SSPANEL ''',就可以进行签到。要确认是否是''' Powered by SSPANEL '''，在机场首页滑倒最底端就可以看到
 #参数	是否必须	内容
-#URL	是	机场地址
-#EMAIL	是	账号邮箱
-#PASSWD	是	账号密码
-#SCKEY	否	Sever酱秘钥
-#TOKEN	否      pushplus密钥
-import requests, json, re, os
+#JC_URL	是	机场地址
+#JC_EMAIL	是	账号邮箱
+#JC_PASSWD	是	账号密码
 
+import requests, json, re, os
+import notify
 session = requests.session()
 # 机场的地址
-url = os.environ.get('URL')
+url = os.environ.get('JC_URL')
 # 配置用户名（一般是邮箱）
-email = os.environ.get('EMAIL')
+email = os.environ.get('JC_EMAIL')
 # 配置用户名对应的密码 和上面的email对应上
-passwd = os.environ.get('PASSWD')
-# server酱
-SCKEY = os.environ.get('SCKEY')
-# PUSHPLUS
-Token = os.environ.get('TOKEN')
-def push(content):
-    if SCKEY != '1':
-        url = "https://sctapi.ftqq.com/{}.send?title={}&desp={}".format(SCKEY, '机场签到', content)
-        requests.post(url)
-        print('推送完成')
-    elif Token != '1':
-        headers = {'Content-Type': 'application/json'}
-        json = {"token": Token, 'title': '机场签到', 'content': content, "template": "json"}
-        resp = requests.post(f'http://www.pushplus.plus/send', json=json, headers=headers).json()
-        print('push+推送成功' if resp['code'] == 200 else 'push+推送失败')
-    else:
-        print('未使用消息推送推送！')
-
+passwd = os.environ.get('JC_PASSWD')
 
 login_url = '{}/auth/login'.format(url)
 check_url = '{}/user/checkin'.format(url)
@@ -53,8 +35,8 @@ try:
     print(result['msg'])
     content = result['msg']
     # 进行推送
-    push(content)
+    notify.send(url,content)
 except:
-    content = '机场签到失败'
+    content = '签到失败'
     print(content)
-    push(content)
+    notify.send(url,content)
